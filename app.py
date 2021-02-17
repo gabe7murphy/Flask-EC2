40a8be36c3d62ab8abf1956637b5a3053af17c7a
Sam-Dominguez <samdominguez@gmail.com>
2021-02-16 21:20:49 -0500

    new commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+@app.route('/home')
+def home():
     return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
